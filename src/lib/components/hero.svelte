dc662c1de002fb412a997768bad5a27033477c19
Jim Burbridge <jhechtf@gmail.com>
2023-09-25 19:47:31 -0700

    chore: initial commit

---
+++ src/lib/components/hero.svelte
@@ -0,0 +1,7 @@
+<section class="h-screen bg-slate-700 flex justify-center p-8 gap-8 flex-col items-center">
+  <p class="text-6xl font-bold">Hello, I am the hero</p>
+  <p>Other stuff</p>
+  <div class="container bg-orange-400 p-4 rounded">
+    hi you
+  </div>
+</section>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Hero from "$lib/components/hero.svelte";
+</script>
+
+<svelte:head>
+  <title>Sungmanito</title>
+</svelte:head>
+
+<Hero />
+
+<div class="container mx-auto mt-8">
+  <h1 class="text-4xl font-bold mb-8">
+    Sungmanito, 
+    <small><em>Noun</em></small>
+  </h1>
+
+  <div>
+    <p class="text-2xl font-semibold">
+      The Lakota Wolf god considered the source and patron of the hunt.
+    </p>
+  </div>
+</div>
