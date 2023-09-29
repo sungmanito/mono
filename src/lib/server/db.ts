b181f6c2853ba4ecdba3c42dfc26537c0cb3eb43
Jim Burbridge <jhechtf@gmail.com>
2023-09-28 23:50:33 -0700

    feat: updates the DB definitions

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,7 +1,8 @@
+import { relations } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { DB_URL } from '$env/static/private';
-import { pgTable, text, date, integer } from 'drizzle-orm/pg-core';
+import { pgTable, text, date, integer, pgSchema, uuid, varchar, index } from 'drizzle-orm/pg-core';
 
 const client = postgres(DB_URL)
 
@@ -24,6 +25,28 @@ export const Households = pgTable(
   }
 );
 
+export const usersToHouseholds = pgTable('users_to_households', {
+  id: uuid('id').primaryKey(),
+  userId: uuid('user_id').notNull(),
+  householdId: text('household_id').notNull(),
+}, ({ userId, householdId }) => {
+  return {
+    userIdIndex: index('users_households_user_index').on(userId),
+    householdIdIndex: index('users_households_household_index').on(householdId),
+  }
+});
+
+export const usersToHouseholdsRelations = relations(usersToHouseholds, ({ one }) => ({
+  user: one(Users, {
+    fields: [usersToHouseholds.userId],
+    references: [Users.id]
+  }),
+  houseHhold: one(Households, {
+    fields: [usersToHouseholds.householdId],
+    references: [Households.id]
+  })
+}));
+
 export const Payments = pgTable(
   'payments',
   {
@@ -34,5 +57,28 @@ export const Payments = pgTable(
     createdAt: date('created_at').notNull().defaultNow(),
   }
 );
+
+const authSchema = pgSchema('auth');
+
+export const Users = authSchema.table(
+  'users',
+  {
+    id: uuid('id').primaryKey(),
+    email: varchar('email')
+  },
+)
+
+export const householdUsers = relations(Households, ({ many }) => ({
+  users: many(Users),
+}))
+
+export const userHouseHolds = relations
+
+export const paymentRelations = relations(Payments, ({ one }) => ({
+  bill: one(Bills, {
+    fields: [Payments.billId],
+    references: [Bills.id]
+  })
+}));
 
 export const db = drizzle(client);
