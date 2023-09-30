8ae26a6ebdf82244fb1cb5f48829810d2344e264
Jim Burbridge <jhechtf@gmail.com>
2023-09-30 13:00:38 -0700

    wip: Syncing

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from 'drizzle-kit';
+import 'dotenv/config';
 export default {
   schema: './src/lib/server/db/schema.ts',
-  out: './drizzle'
+  out: './drizzle',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DB_URL,
+  }
 } satisfies Config;
