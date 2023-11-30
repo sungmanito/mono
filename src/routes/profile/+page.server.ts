a265b5b9ae8fa79af4e5baf99de4059e364f85b9
Jim Burbridge <jhechtf@gmail.com>
2023-11-30 09:59:01 -0800

    wip

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,6 +1,8 @@
+
 export const actions = {
   updateProfileData: async ({ locals }) => {
-  }
-}
+    
+  },
+};
 
 export const ssr = false;
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -5,6 +5,10 @@
   import { MailIcon } from 'lucide-svelte';
   import type { User } from '@supabase/supabase-js';
   import GoogleIcon from '$lib/components/googleIcon.svelte';
+  import Identity from './_components/identities/identity.svelte';
+  import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+  import Button from '$lib/components/button/button.svelte';
+
   export let data;
 
   $: if(!$page.data.user.id) {
@@ -15,26 +19,46 @@
     if(user.user_metadata.name) return user.user_metadata.name;
     return user.email;
   }
+
+  let editProfile = false;
 </script>
+
+<svelte:head>
+  <title>
+    Sungmanito &ndash; User Profile
+  </title>
+</svelte:head>
+
+<Drawer open={editProfile} on:clo>
+  <Header color="secondary" tag="h2">
+    Edit Profile
+  </Header>
+</Drawer>
+
 <section class="flex-grow">
   <div class="container mx-auto">
     <Header class="mt-4">
       Profile
+      <svelte:fragment slot="actions">
+        <Button on:click={() => editProfile = true}>
+
+        </Button>
+      </svelte:fragment>
     </Header>
     {#if data.user}
     <div class="grid grid-cols-12 gap-3">
-      <div class="col-span-12">
+      <div class="col-span-1">
         {#if data.user.user_metadata?.avatar_url}
-          <img src={data.user.user_metadata.avatar_url} class="rounded-full" alt="Profile image"/>
+          <img src={data.user.user_metadata.avatar_url} class="rounded-full" alt="profile for"/>
         {/if}
       </div>
 
-      <div class="flex gap-2 text-lg text-zinc-400 col-span-2">
+      <div class="flex flex-col gap-2 text-lg text-zinc-400 col-span-2">
         <span class="font-bold">ID</span>
-        <span>{data.user.id}</span>
+        <span class="text-sm">{data.user.id}</span>
       </div>
       
-      <div class="flex gap-2 text-lg col-span-2">
+      <div class="flex flex-col gap-2 text-lg col-span-2">
         <div class="font-bold">
           User name
         </div>
@@ -43,7 +67,7 @@
         </span>
       </div>
 
-      <div class="flex gap-2 text-lg col-span-2">
+      <div class="flex flex-col gap-2 text-lg col-span-2">
         <div class="font-bold">
           Email
         </div>
@@ -52,42 +76,14 @@
         </span>
       </div>
 
-      <div class="col-start-1 col-span-12 border-t p-3">
+      <div class="col-start-1 col-span-12 border-t p-3 flex flex-col gap-3">
         <Header tag="h2" color="secondary">
           Identities
         </Header>
 
         {#if data.user.identities}
           {#each data.user.identities as identity}
-            <div class="flex gap-3 variant-filled-surface rounded p-4">
-              {#if identity.provider === 'google'}
-                <GoogleIcon class="w-8 h-8" />
-              {:else if identity.provider === 'email'}
-                <MailIcon class="w-8 h-8"/>
-              {/if}
-              <div>
-                <div>
-                  Identity ID: {identity.id}
-                </div>
-                {#if identity.identity_data?.full_name}
-                <div>
-                  Full Name: {identity.identity_data?.full_name}
-                </div>
-                {/if}
-                {#if identity.identity_data?.avatar_url}
-                <div>
-                  Image
-                  <img src={identity.identity_data.avatar_url} class="rounded" alt={`${identity.provider} avatar`}/>
-                </div>
-                {/if}
-                <div>
-                  Last sign in:
-                  {identity.last_sign_in_at}
-                </div>
-              </div>
-            </div>
-
-            {JSON.stringify(identity)}
+            <Identity identity={identity}/>
           {/each}
         {/if}
       </div>
