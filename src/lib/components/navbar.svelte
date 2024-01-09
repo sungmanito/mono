a255048e2fbb3c516d56c21b1870c70c5cdfdc58
Jim Burbridge <jhechtf@gmail.com>
2024-01-08 22:59:21 -0800

    feat: add in sungmanito logo (#18)

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -4,10 +4,13 @@
   import { LayoutDashboardIcon, User2Icon } from 'lucide-svelte';
 </script>
 <AppBar shadow="shadow">
-	<svelte:fragment slot="lead">
-    Sungmanito
+  <svelte:fragment slot="lead">
+    <div class="flex gap-2">
+      <img src="/sungmanito-color.svg" alt="Sungmanito Logo of a wolf" class="h-[1.75em]">
+      Sungmanito
+    </div>
   </svelte:fragment>
-	<svelte:fragment slot="trail">
+  <svelte:fragment slot="trail">
     <LightSwitch />
     {#if $page.data.user && $page.data.user.id !== undefined}
       <a class="btn btn-sm variant-filled flex gap-1" href="/logout">
