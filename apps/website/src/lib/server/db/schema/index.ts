660e66de5a48125b70f9fa3a3af97370180dd1d6
Jim Burbridge <jhechtf@gmail.com>
2024-01-19 11:32:51 -0800

    fafo

--- apps/website/src/lib/server/db/schema/index.ts
+++
@@ -1,8 +0,0 @@
-// Please hold
-export * from './households.table';
-export * from './payments.table';
-export * from './users.table';
-export * from './usersToHouseholds.table';
-export * from './bills.table';
-export * from './identities.table';
-export * from './invites.table';
