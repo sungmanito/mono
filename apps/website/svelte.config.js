80787899f412b66b16a481a6767b7a5db863984c
Jim Burbridge <jhechtf@gmail.com>
2024-01-22 21:23:58 -0800

    chore: Add in CSP for sentry stuff (#32)

--- apps/website/svelte.config.js
+++ apps/website/svelte.config.js
@@ -14,6 +14,12 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    csp: {
+      directives: {
+        'worker-src': ['self', 'blob:'],
+        'child-src': ['self', 'blob:'],
+      },
+    },
   },
 };
 
