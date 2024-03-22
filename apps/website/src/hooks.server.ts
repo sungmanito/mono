cb990327ee32b038ae270e3749700594fcd82955
Jim Burbridge <jhechtf@gmail.com>
2024-03-21 18:03:38 -0700

    formatter

--- apps/website/src/hooks.server.ts
+++ apps/website/src/hooks.server.ts
@@ -1,7 +1,7 @@
 import { EDGE_CONFIG, SUPABASE_SERVICE_ROLE } from '$env/static/private';
 import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { db } from '$lib/server/db';
-import { eq, and} from 'drizzle-orm';
+import { eq, and } from 'drizzle-orm';
 import schema from '@sungmanito/db';
 // import { getUserHouseholds } from '$lib/server/actions/households.actions';
 import { validateUserSession } from '$lib/util/session';
@@ -14,66 +14,64 @@ import { getAll } from '@vercel/edge-config';
 // but the vercel
 process.env.EDGE_CONFIG = EDGE_CONFIG;
 
-export const handle: Handle = sequence(
-  async ({ event, resolve }) => {
-    /**
-     * Creates a supabase server client using some ENV variables.
-     * This is an admin-level client, so be careful when calling any deletes.
-     */
-    const supabase = createServerClient(
-      PUBLIC_SUPABASE_URL,
-      SUPABASE_SERVICE_ROLE,
-      {
-        cookies: {
-          get: (key) => event.cookies.get(key),
-          set: (key, value, options) => {
-            event.cookies.set(key, value, options);
-          },
-          remove: (key, options) => {
-            event.cookies.delete(key, options);
-          },
+export const handle: Handle = sequence(async ({ event, resolve }) => {
+  /**
+   * Creates a supabase server client using some ENV variables.
+   * This is an admin-level client, so be careful when calling any deletes.
+   */
+  const supabase = createServerClient(
+    PUBLIC_SUPABASE_URL,
+    SUPABASE_SERVICE_ROLE,
+    {
+      cookies: {
+        get: (key) => event.cookies.get(key),
+        set: (key, value, options) => {
+          event.cookies.set(key, value, options);
+        },
+        remove: (key, options) => {
+          event.cookies.delete(key, options);
         },
       },
-    );
+    },
+  );
 
-    /**
-     * This is from the Vercel Edge Config
-     */
-    event.locals.config = await getAll<App.VercelConfig>();
+  /**
+   * This is from the Vercel Edge Config
+   */
+  event.locals.config = await getAll<App.VercelConfig>();
 
-    // Passes this on to the locals so that load functions can use it later
-    event.locals.supabase = supabase;
+  // Passes this on to the locals so that load functions can use it later
+  event.locals.supabase = supabase;
 
-    // Wrapper function to make getting the user session easier
-    event.locals.getSession = async () => {
-      const {
-        data: { session },
-      } = await event.locals.supabase.auth.getSession();
-      return session;
-    };
+  // Wrapper function to make getting the user session easier
+  event.locals.getSession = async () => {
+    const {
+      data: { session },
+    } = await event.locals.supabase.auth.getSession();
+    return session;
+  };
 
-    const session = await event.locals.getSession();
+  const session = await event.locals.getSession();
 
-    // Quick and easy way to protect the dashboard.
-    if (session === null && event.url.pathname.startsWith('/dashboard')) {
-      redirect(303, `/login?url=${event.url.pathname}`);
-    }
+  // Quick and easy way to protect the dashboard.
+  if (session === null && event.url.pathname.startsWith('/dashboard')) {
+    redirect(303, `/login?url=${event.url.pathname}`);
+  }
 
-    // We are gathering the logged in users' households a lot
-    // To hopefully save that, we store them in the locals.
-    if (validateUserSession(session)) {
-      console.info('Gathering user households');
-      event.locals.userHouseholds = [];
-    } else {
-      event.locals.userHouseholds = [];
-    }
+  // We are gathering the logged in users' households a lot
+  // To hopefully save that, we store them in the locals.
+  if (validateUserSession(session)) {
+    console.info('Gathering user households');
+    event.locals.userHouseholds = [];
+  } else {
+    event.locals.userHouseholds = [];
+  }
 
-    return resolve(event, {
-      filterSerializedResponseHeaders(name) {
-        return name === 'content-range';
-      },
-    });
-  },
-);
+  return resolve(event, {
+    filterSerializedResponseHeaders(name) {
+      return name === 'content-range';
+    },
+  });
+});
 
 // export const handleError = Sentry.handleErrorWithSentry();
