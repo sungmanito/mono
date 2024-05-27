8decd1362cfc55a0de3dbc433e7fc5ac88f6838d
Jim Burbridge <jhechtf@gmail.com>
2024-05-26 22:09:24 -0700

    feat: move to skeleton 3

---
+++ apps/website/src/lib/components/drawerify/drawerify.svelte
@@ -0,0 +1,11 @@
+<script lang="ts" generics="T">
+  import { preloadData } from '$app/navigation';
+  import Drawer from '$lib/components/drawer/drawer.svelte';
+  export let route: string;
+  export let open = false;
+  export let onclose: () => void = () => void 0;
+</script>
+
+<Drawer open on:close={onclose} let:close={drawerClose}>
+  <slot component={true} />
+</Drawer>
