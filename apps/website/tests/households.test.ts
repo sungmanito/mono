a79c49704899bfd566c3e52d5e6b7ff97745ea69
Jim Burbridge <jhechtf@gmail.com>
2024-06-24 19:35:41 -0700

    tests: Add in more thorough tests for Households

--- apps/website/tests/households.test.ts
+++ apps/website/tests/households.test.ts
@@ -14,7 +14,7 @@ test('Navigating and logging in redirection works', async ({ page }) => {
   expect(page.url()).toMatch('/dashboard/household');
 });
 
-test('User can create household', async ({ page }) => {
+test('User can create household through dialog', async ({ page }) => {
   await page.goto('/');
   await page.goto('/dashboard/household');
   await login(page);
@@ -23,6 +23,58 @@ test('User can create household', async ({ page }) => {
     .filter({ hasText: /^Add$/ })
     .getByRole('button')
     .click();
-  // await page.getByRole('button', { name: 'Add' }).click();
   await expect(page.getByText('New household')).toBeVisible();
+  await page.getByLabel('Household Name').fill('Household 1');
+  await page.getByRole('dialog').getByRole('button', { name: 'Add' }).click();
+  await expect(page.getByRole('dialog')).not.toBeVisible();
+  await expect(
+    page.getByTestId('sidebar-household').getByText('Household 1'),
+  ).toBeVisible();
+});
+
+test('User can view household details', async ({ page }) => {
+  await page.goto('/');
+  await page.goto('/dashboard/household');
+  await login(page);
+
+  await page.getByTestId('sidebar-household').getByText('Household 1').click();
+  await expect(
+    page.getByRole('heading', { name: 'Household 1' }),
+  ).toBeInViewport();
+});
+
+test('User can edit', async ({ page }) => {
+  await page.goto('/');
+  await page.goto('/dashboard/household');
+  await login(page);
+
+  await page.getByTestId('sidebar-household').getByText('Household 1').click();
+
+  await page.getByRole('button', { name: 'Edit' }).click();
+  await page.getByLabel('Household Name').clear();
+  await page.getByLabel('Household Name').fill('Edited Household 1');
+  await page.getByRole('dialog').getByRole('button', { name: 'Save' }).click();
+  await expect(
+    page.getByTestId('sidebar-household').getByText('Edited Household 1'),
+  ).toBeVisible();
+});
+
+test('User can delete household', async ({ page }) => {
+  await page.goto('/');
+  await page.goto('/dashboard/household');
+  await login(page);
+
+  await page
+    .getByTestId('sidebar-household')
+    .getByText('Edited Household 1')
+    .click();
+  await page.getByRole('button', { name: 'Delete' }).click();
+  await page.getByRole('dialog').getByRole('textbox').fill('delete');
+  await page
+    .getByRole('dialog')
+    .getByRole('button', { name: 'Delete' })
+    .click();
+  await expect(
+    page.getByTestId('sidebar-household').getByText('Edited Household 1'),
+  ).not.toBeVisible();
 });
