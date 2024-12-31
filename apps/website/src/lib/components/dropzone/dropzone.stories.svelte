accf19e51e076145c6cc0953365cda0496546f08
Jim Burbridge <jhechtf@gmail.com>
2024-12-30 21:07:20 -0800

    chore: Move Dropzone stories to Storybook

---
+++ apps/website/src/lib/components/dropzone/dropzone.stories.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Dropzone from './dropzone.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Dropzone',
+    component: Dropzone,
+    tags: ['autodocs'],
+  });
+</script>
+
+{#snippet defaultChildren()}
+  Drag an image over me
+{/snippet}
+
+{#snippet dragover()}
+  I am shown when something is being dragged over the item
+{/snippet}
+
+<Story name="Default" args={{ children: defaultChildren }} />
+
+<Story name="With Dragover" args={{ children: defaultChildren, dragover }} />
