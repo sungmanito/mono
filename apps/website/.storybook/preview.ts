8de24b1a65eb2e9211d2784804f55c08cd73eacf
Jim Burbridge <jhechtf@gmail.com>
2025-01-10 15:44:44 -0800

    test(Button): Adds extra stories and a minor unit test

--- apps/website/.storybook/preview.ts
+++ apps/website/.storybook/preview.ts
@@ -1,5 +1,6 @@
-import type { Preview } from '@storybook/svelte';
+import type { Preview, SvelteRenderer } from '@storybook/svelte';
 import './stories.css';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +11,16 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    withThemeByDataAttribute<SvelteRenderer>({
+      themes: {
+        sungmanito: 'sungmanito',
+      },
+      defaultTheme: 'sungmanito',
+      parentSelector: 'body',
+      attributeName: 'data-theme',
+    }),
+  ],
 };
 
 export default preview;
