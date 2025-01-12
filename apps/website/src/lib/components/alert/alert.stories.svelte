60f1fbb1db9efdab9f577666d27930d164b7c3c9
Jim Burbridge <jhechtf@gmail.com>
2025-01-12 14:08:52 -0800

    feat: Add in Alert component,No tests as it's almost entirely a display-based component

---
+++ apps/website/src/lib/components/alert/alert.stories.svelte
@@ -0,0 +1,28 @@
+<script lang="ts" module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import { AlertTriangleIcon, XIcon } from 'lucide-svelte';
+  import Alert from './alert.svelte';
+  const { Story } = defineMeta({
+    title: 'Alerts',
+    component: Alert,
+    tags: ['autodocs'],
+    args: {
+      type: 'default',
+    },
+  });
+</script>
+
+<Story name="Default">
+  {#snippet children(args)}
+    <Alert {...args}>
+      <div class="flex items-center gap-6">
+        <AlertTriangleIcon size="2em" /> Alert without thingamajig
+      </div>
+      {#snippet actions()}
+        <button class="hover:rounded-full hover:bg-secondary-hover-token p-2">
+          <XIcon size="1.5em" />
+        </button>
+      {/snippet}
+    </Alert>
+  {/snippet}
+</Story>
---
+++ apps/website/src/lib/components/alert/alert.svelte
@@ -0,0 +1,55 @@
+<script lang="ts" module>
+  import { cva, type VariantProps } from 'class-variance-authority';
+  import { type Snippet } from 'svelte';
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  const variants = cva('alert', {
+    variants: {
+      type: {
+        primary: 'variant-filled-primary',
+        error: 'variant-filled-error',
+        default: 'variant-filled',
+        surface: 'variant-filled-surface',
+        warning: 'variant-filled-warning',
+        success: 'variant-filled-success',
+        'primary:ghost': 'variant-ghost-primary',
+        'error:ghost': 'variant-ghost-error',
+        'surface:ghost': 'variant-ghost-surface',
+        'warning:ghost': 'variant-ghost-warning',
+        'success:ghost': 'variant-ghost-success',
+      },
+    },
+    defaultVariants: {
+      type: 'default',
+    },
+  });
+
+  export type VariantTypes = VariantProps<typeof variants>;
+
+  export interface AlertProps extends HTMLAttributes<HTMLElement> {
+    children: Snippet<[]>;
+    actions?: Snippet<[]>;
+  }
+</script>
+
+<script lang="ts">
+  let {
+    children,
+    actions,
+    type = 'default',
+    class: className,
+    ...rest
+  }: AlertProps & VariantTypes = $props();
+  let classes = $derived(variants({ type }));
+</script>
+
+<div class={classes} {...rest}>
+  <div class="alert-message">
+    {@render children()}
+  </div>
+  {#if actions}
+    <div class="alert-actions">
+      {@render actions()}
+    </div>
+  {/if}
+</div>
