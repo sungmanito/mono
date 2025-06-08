9e44d893f9f47448f7b395f541fd17c94ad8f2f4
Jim Burbridge <jhechtf@gmail.com>
2025-06-08 11:02:14 -0700

    wip - update later

---
+++ apps/website/src/lib/components/buttonGroup/buttonGroup.stories.svelte
@@ -0,0 +1,11 @@
+<script lang="ts" module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import ButtonGroup from './buttonGroup.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Components/ButtonGroup',
+    component: ButtonGroup,
+  });
+</script>
+
+<Story name="Default" />
