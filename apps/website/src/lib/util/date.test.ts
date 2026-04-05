37165acc4b32421d1420ce5987cc79b6008caf8d
Jim Burbridge <jhechtf@gmail.com>
2026-04-05 01:02:23 -0700

    feat: Add getLastDayOfMonth utility with tests (#139)

--- apps/website/src/lib/util/date.test.ts
+++ apps/website/src/lib/util/date.test.ts
@@ -1,11 +1,18 @@
-import { describe, it } from 'vitest';
-import { getFullTimeBetween } from './date';
+import { describe, expect, it } from 'vitest';
+import { getFullTimeBetween, getLastDayOfMonth } from './date';
 
 describe('Date utils', () => {
   const yesterday = new Date();
   yesterday.setDate(yesterday.getDate() - 1);
-  it('Does the thing', () => {
+  it('Gets the file time between dates', () => {
     const resp = getFullTimeBetween(yesterday);
     console.info(resp);
+  });
+
+  it('Gets the last date of a given month', () => {
+    const leapYearFeb = new Date(2024, 1, 14);
+    const regFeb = new Date(2026, 1, 14);
+    expect(getLastDayOfMonth(leapYearFeb)).toBe(29);
+    expect(getLastDayOfMonth(regFeb)).toBe(28);
   });
 });
